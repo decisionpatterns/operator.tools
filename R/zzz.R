15daa778bfa01691e371cf39f17b1d423daa858d
Christopher Brown <cbrown@decisionpatterns.com>
2015-02-05 10:33:38 -0800

    Update copyright onAttach message

--- R/zzz.R
+++ R/zzz.R
@@ -2,18 +2,19 @@
 # .First.lib <- function( libname, pkgname ) {
 .onAttach <- function( libname, pkgname ) {
 
+  if( interactive() )
+    packageStartupMessage( 
+      pkgname ,
+      "-" ,
+      utils::packageVersion(pkgname, libname),
+      " - Copyright \u00a9 ", substr(Sys.Date(),1,4),
+      " Decision Patterns" ,
+      domain = NA
+    )
 
-  packageStartupMessage( 
-    pkgname ,
-    "-" ,
-    utils::packageVersion(pkgname, libname),
-    " provided by Decision Patterns" ,
-    domain = NA
-  )
 
   # Initializes package
   .initOps()
 
 }
-
 
